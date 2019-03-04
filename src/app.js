8953546e51481c1b534310f8a3132feffab6f0a2
wangliuyong <1355498705@qq.com>
2019-03-04 18:19:08 +0800

    验证iconPosition为right或者left

--- src/app.js
+++ src/app.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 
 import Button from './button.vue'
 
-Vue.component('g-button',Button)   //注册全局的组件
+
+Vue.component('g-button', Button)   //注册全局的组件
 
 const App = new Vue({
-    el:"#app"
+    el: "#app"
 })
--- src/button.vue
+++ src/button.vue
@@ -1,36 +1,88 @@
 <template>
-    <button class="g-button">
-        <slot></slot>
-    </button>
-</template>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+    <svg v-if="icon" class="icon" aria-hidden="true">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
 
+    <div class="content">
+      <slot></slot>
+    </div>
+    <!-- /.content -->
+
+
+  </button>
+</template>
 
 <script>
     export default {
+        props: {
+            icon:{},
+            iconPosition:{
+                type:String,
+                default:'left',
+                /* 自定义验证*/
+                validator(value){
+                    return !(value !== 'right' && value !== 'left');
+                }
+            }
+        },
 
+
+        created: function () {
+            console.log('props', this.icon);
+        }
     }
 </script>
 
 
-
 <style lang="scss">
-    .g-button {
-        font-size: var(--font-size);
-        height: var(--button-height);
-        padding: 0 1em;
-        border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
-        background: var(--button-bg);
+  .g-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
+    line-height: 20px;
 
-        &:hover {
-            border-color: var(--border-color-hover);
-        }
-        &:active {
-            background-color: var(--button-active-bg);
-        }
-        &:focus {
-            outline: none;
-        }
+    &:hover {
+      border-color: var(--border-color-hover);
     }
+
+    &:active {
+      background-color: var(--button-active-bg);
+    }
+
+    &:focus {
+      outline: none;
+    }
+
+    .icon {
+      order: 1;
+      margin-right: .2em;
+    }
+
+    .content {
+      order: 2;
+    }
+
+    &.icon-right {
+      .icon {
+        order: 2;
+        margin-left: .2em;
+      }
+
+      .content {
+        order: 1;
+      }
+
+    }
+
+
+  }
 
 </style>
