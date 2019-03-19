c3be37f04a28ec34f6456599919dccf80e76c482
wly <1355498705@qq.com>
2019-03-19 20:47:58 +0800

    tarn add chai

--- src/app.js
+++ src/app.js
@@ -3,12 +3,19 @@ import Vue from 'vue'
 import Button from './button.vue'
 
 import ButtonGroup from './button-group.vue'
-
+import chai from 'chai'
 
 //注册全局的组件
 Vue.component('g-button', Button)
 Vue.component('g-button-group', ButtonGroup)
 
 const App = new Vue({
-    el: "#app",
+  el: "#app",
 })
+
+const Constructor=Vue.extend(Button)
+
+
+
+
+console.log(Constructor)
--- src/button-group.vue
+++ src/button-group.vue
@@ -1,50 +1,50 @@
 <template>
-  <div id="g-button-group">
-    <slot></slot>
-  </div>
-  <!-- /#button-group -->
+    <div id="g-button-group">
+        <slot></slot>
+    </div>
+    <!-- /#button-group -->
 </template>
 
 <script>
-    export default {
-        name: "g-button-group",
-        mounted: function () {
-            for (let node of this.$el.children) {
-                //console.log(node.nodeName);
-                let name = node.nodeName
-                if (name !== 'BUTTON') {
-                    console.warn(`button-group的子元素应该是button,但是你的子元素是${name.toLocaleLowerCase()}`)
-                }
-            }
+  export default {
+    name: "g-button-group",
+    mounted: function () {
+      for (let node of this.$el.children) {
+        //console.log(node.nodeName);
+        let name = node.nodeName
+        if (name !== 'BUTTON') {
+          console.warn(`button-group的子元素应该是button,但是你的子元素是${name.toLocaleLowerCase()}`)
         }
-    }
-</script>
-
-<style scoped lang="scss">
-  #g-button-group {
-    display: inline-flex;
-    vertical-align: middle;
-
-    > .g-button {
-      border-radius: 0;
-      margin-left: -1px;
-
-      &:nth-child(1) {
-        margin-left: 0;
-        border-top-left-radius: var(--border-radius);
-        border-bottom-left-radius: var(--border-radius);
-      }
-
-      &:nth-child(3) {
-        border-top-right-radius: var(--border-radius);
-        border-bottom-right-radius: var(--border-radius);
-      }
-
-      &:hover {
-        position: relative;
-        z-index: 2;
-        border-color: red;
       }
     }
   }
+</script>
+
+<style scoped lang="scss">
+    #g-button-group {
+        display: inline-flex;
+        vertical-align: middle;
+
+        > .g-button {
+            border-radius: 0;
+            margin-left: -1px;
+
+            &:nth-child(1) {
+                margin-left: 0;
+                border-top-left-radius: var(--border-radius);
+                border-bottom-left-radius: var(--border-radius);
+            }
+
+            &:nth-child(3) {
+                border-top-right-radius: var(--border-radius);
+                border-bottom-right-radius: var(--border-radius);
+            }
+
+            &:hover {
+                position: relative;
+                z-index: 2;
+                border-color: red;
+            }
+        }
+    }
 </style>
