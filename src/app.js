f4965f9b77d8d254099dc76ff8cc18060f5eae75
王刘永 <1355498705@qq.com>
2020-02-21 10:43:09 +0800

    组件分类文件夹

--- src/app.js
+++ src/app.js
@@ -1,21 +1,16 @@
-import Vue from 'vue'
+import Vue from 'vue';
+import Button from './component/button.vue';
+import ButtonGroup from './component/button-group.vue';
 
-import Button from './button.vue'
-
-import ButtonGroup from './button-group.vue'
-import chai from 'chai'
 
 //注册全局的组件
-Vue.component('g-button', Button)
-Vue.component('g-button-group', ButtonGroup)
+Vue.component('g-button', Button);
+Vue.component('g-button-group', ButtonGroup);
 
 const App = new Vue({
   el: "#app",
-})
+});
 
-const Constructor=Vue.extend(Button)
+const Constructor=Vue.extend(Button);
 
-
-
-
-console.log(Constructor)
+console.log(Constructor);
